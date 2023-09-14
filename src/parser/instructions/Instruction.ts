695d02057a1243697c993b9516f1819ea3483964
MikhailArkhipov <mikhaila@outlook.com>
2023-09-13 22:08:22 -0700

    Instruction syntacx, partial III

--- src/parser/instructions/Instruction.ts
+++ src/parser/instructions/Instruction.ts
@@ -7,16 +7,15 @@ import { ParseError } from '../parseError';
 export interface Instruction {
   readonly fullName: string; // LDMIANE.W
   readonly name: string; // 'LDM' - core name
-  readonly suffix: string; // 'S' as in ADD/ADDS
-  readonly type: string; // 'IA' - type, mode, effect: instruction specific
+    // 'S' as in ADD/ADDS or 'IA' - type, mode, effect: instruction specific
+  readonly suffix: string;
   readonly condition: string; // NE/Z/...
   readonly modifier: string; // .W or .N
   readonly errors: ParseError[];
 
-  readonly allowsSuffix: boolean;
-  readonly allowsModifier: boolean;
-  readonly allowedTypes: string[];
-  readonly operands: string;
+  readonly allowedModifiers: string[];
+  readonly allowedSuffixes: string[];
+  readonly allowedOperands: string;
 }
 
 export class InstructionImpl implements Instruction {
@@ -25,15 +24,14 @@ export class InstructionImpl implements Instruction {
   public readonly errors: ParseError[] = [];
 
   public name: string = ''; // 'LDM' - core name
-  public suffix: string = ''; // 'S' as in ADD/ADDS
-  public type: string = ''; // 'IA' - type, mode, effect: instruction specific
+  // 'S' as in ADD/ADDS or 'IA' - type, mode, effect: instruction specific
+  public suffix: string = ''; 
   public condition: string = ''; // NE/Z/...
   public modifier: string = ''; // .W or .N
 
-  public allowsSuffix: boolean; // Allows 'S' suffix like ADD/ADDS
-  public allowsModifier: boolean; // Allows modifier like .W
-  public allowedTypes: string[] = []; // Allowed types or effects, like CPS/CPSIE/CPSID
-  public operands: string; // Operand syntax, like "*" means any, "RRO" = 'Rd, Rn, Op'.
+  public allowedModifiers: string[]; // Allowed modifiers like .W or .T
+  public allowedSuffixes: string[] = []; // Allowed types or effects, like CPS/CPSIE/CPSID
+  public allowedOperands: string; // Operand syntax, like "*" means any, "RRO" = 'Rd, Rn, Op'.
 
   constructor(fullName: string, range: TextRange) {
     this.fullName = fullName;
