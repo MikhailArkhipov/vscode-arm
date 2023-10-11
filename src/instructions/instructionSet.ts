8353f389b4827c151bb4600d069fcf6053e7aa1a
MikhailArkhipov <mikhaila@outlook.com>
2023-10-10 18:50:57 -0700

    Match casing for publish

--- src/instructions/instructionSet.ts
+++ src/instructions/instructionSet.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Mikhail Arkhipov. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-import * as a32set from '../instruction_sets/A32.json';
-import * as a64set from '../instruction_sets/A64.json';
+import * as a32set from '../instruction_sets/a32.json';
+import * as a64set from '../instruction_sets/a64.json';
 
 import { A32Set } from '../tokens/definitions';
 
